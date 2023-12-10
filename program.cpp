d02a2d36cd3d4adf25e8f7dcb84f109743b867c3
Yaroslav <yarik.peresypkin4070@gmail.com>
2023-12-10 23:46:54 +0300

    add function MyPow() version2

--- program.cpp
+++ program.cpp
@@ -8,6 +8,7 @@ cout << MySum(10, 3) << endl;
 cout << MySub(10, 3) << endl;
 cout << MyMul(10, 3) << endl;
 cout << MyDuv(10, 3) << endl;
+cout << MyPow(10.5, 3.7) << endl;
 system("pause");
 return 0;
 }
